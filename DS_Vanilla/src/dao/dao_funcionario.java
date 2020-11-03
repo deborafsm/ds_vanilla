26d478e878a612ab5a07e511e4b8529944f02ad8
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2020-11-03 15:40:59 -0300

    Funcionario

--- DS_Vanilla/src/dao/dao_funcionario.java
+++ DS_Vanilla/src/dao/dao_funcionario.java
@@ -9,6 +9,7 @@ import ds_vanilla.dsVanilla_ConnectionFactory;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import javax.swing.JOptionPane;
 import model.model_pessoa;
 
 /**
@@ -28,26 +29,43 @@ public class dao_funcionario {
     public void insert(model_pessoa pessoa){//adiciona salario 
         Connection con = dsVanilla_ConnectionFactory.getConnection();
         PreparedStatement ps = null;
-        try {
-            ps= con.prepareStatement("insert into ()");
+        try {                           //INSERT INTO vagas (data_vaga, nome_empregador, titulo, descricao, celular, telefone, email)VALUES(?,?,?,?,?,?,?)
+            ps= con.prepareStatement("INSERT INTO (nome, celular, email,tipo_contrato, endereco, cargo, salario, status_funcionario)VALUES(?,?,?,?,?,?,?,?,?,?)");
+            
             ps.setString(1, pessoa.getNome());
-            ps.setString(2, pessoa.getNome());
-            ps.setString(3, pessoa.getNome());
-            ps.setString(4, pessoa.getNome());
-            ps.setString(5, pessoa.getNome());
-            ps.setString(6, pessoa.getNome());
+            ps.setString(2, pessoa.getCelular());
+            ps.setString(3, pessoa.getEmail());
+            ps.setString(4, pessoa.getTipo_contrato());
+            ps.setString(5, pessoa.getEndereco());
+            ps.setString(6, pessoa.getCargo());
+            ps.setString(7, pessoa.getSalario());
+            ps.setString(8, pessoa.getStatus());
+            ps.executeUpdate();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"Erro"+e);
+        }finally{
+            dsVanilla_ConnectionFactory.closeConnection(con);
+        }
+    }
+    public void update(model_pessoa pessoa){//atualiza salario e status
+        try {
             
         } catch (Exception e) {
-            
         }finally{
             
         }
     }
-    public void update(model_pessoa pessoa){//atualiza salario e status
-        
-    }
     public void remove(model_pessoa pessoa){//remove salário
-        
+        PreparedStatement ps = null;
+        String sql = "";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, pessoa.getId());
+            ps.executeUpdate();
+            
+        } catch (Exception e) {
+        }
+               
     }
 
 
